1b21bfdc5f14faa9e89dcbc50d5144673ce9001f
frankiennamdi <frankie4real@yahoo.com>
2019-07-14 21:46:18 -0400

    FRANKIE: clean up

--- src/main/java/com/ubiquisoft/evaluation/Validator.java
+++ src/main/java/com/ubiquisoft/evaluation/Validator.java
@@ -2,7 +2,17 @@ package com.ubiquisoft.evaluation;
 
 import com.ubiquisoft.evaluation.domain.Car;
 
+/**
+ * Interface for validators of {@link Car}
+ */
 @FunctionalInterface
 public interface Validator {
+
+  /**
+   * Validates the properties of a car.
+   *
+   * @param car the car to validate
+   * @return true if the properties of the car passes the test of this validator, false otherwise
+   */
   boolean validate(Car car);
 }
--- src/test/java/com/ubiquisoft/evaluation/CarDamagedPartsTest.java
+++ src/test/java/com/ubiquisoft/evaluation/CarDamagedPartsTest.java
@@ -8,8 +8,10 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.junit.Assert.assertThat;
 
 public class CarDamagedPartsTest {
@@ -24,13 +26,15 @@ public class CarDamagedPartsTest {
     car.setYear("year");
 
     List<Part> parts = new ArrayList<>(testSupport.newTires(ConditionType.FLAT, ConditionType.NEW));
-    parts.add(testSupport.newPart(PartType.ENGINE, ConditionType.GOOD));
+    parts.add(testSupport.newPart(PartType.ENGINE, ConditionType.NO_POWER));
     car.setParts(parts);
 
     List<Part> damagedParts = car.getDamagedParts();
-    assertThat(damagedParts.size(), is(1));
-    assertThat(damagedParts.get(0).getType(), is(PartType.TIRE));
-    assertThat(damagedParts.get(0).getCondition(), is(ConditionType.FLAT));
+    assertThat(damagedParts.size(), is(2));
+    assertThat(damagedParts.stream().map(Part::getType).collect(Collectors.toList()),
+            containsInAnyOrder(PartType.TIRE, PartType.ENGINE));
+    assertThat(damagedParts.stream().map(Part::getCondition).collect(Collectors.toList()),
+            containsInAnyOrder(ConditionType.FLAT, ConditionType.NO_POWER));
   }
 
   @Test
